954caf02004e92b75aee25b8760d9ef54b50eccd
richardleung1 <richardl57549@gmail.com>
2020-12-18 14:19:30 -0500

    [Navbar] Add component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Welcome from './components/Welcome';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Welcome />
     </div>
   );
